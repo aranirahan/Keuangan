f398e0b79546e38101fef2bd4c37ad74ff3c0e09
aranirahan <aranirahan@gmail.com>
2019-08-16 13:37:12 +0700

    fix

--- app/src/main/java/com/aranirahan/keuangan/db/DatabaseHelper.kt
+++ app/src/main/java/com/aranirahan/keuangan/db/DatabaseHelper.kt
@@ -5,7 +5,7 @@ import android.database.sqlite.SQLiteDatabase
 import com.aranirahan.keuangan.model.Income
 import org.jetbrains.anko.db.*
 
-class DatabaseHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "IncomeDb", null, 1) {
+class DatabaseHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "KeuanganDb", null, 1) {
     companion object {
         private var instance: DatabaseHelper? = null
 
